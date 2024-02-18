c5755a5715854e90b486d067539e9d145108b69b
Mohaiminul375 <mohaiminul375@gmail.com>
2024-02-18 18:45:05 +0600

    push urgent

--- JS/ticket-book.js
+++ JS/ticket-book.js
@@ -1,16 +1,26 @@
+let serial = 0;
+const bookedSeat = [];
 function bookTicket(seat) {
-    let serial = 0
-    serial++
+  serial++
+  console.log(serial);
+  if(bookedSeat.includes(seat.innerText)){
+    console.log(seat.innerText)
+    return;
+  }
+  seat.style.backgroundColor = '#1DD100'
+  seat.style.color = 'white'
   const fairSummaryTable = document.getElementById("table-body");
   const tableRow = document.createElement("tr");
   tableRow.innerHTML = `
     
-                                        <th>${serial}</th>
-                                        <td>${seat.innerText}</td>
-                                        <td
-                                            class="text-[#03071299]">Economy</td>
-                                        <td>550</td>
+    <th>${serial}</th>
+     <td>${seat.innerText}</td>
+    <td  class="text-[#03071299]">Economy</td>
+   <td>550</td>
                                     
     `;
-   fairSummaryTable.append(tableRow);
+  fairSummaryTable.append(tableRow);
+bookedSeat.push(seat.innerText);
+console.log(bookedSeat)
+  
 }
